8975011f6f4ccf96906f636ef9309a4f2d964a17
Umar Waliyullah <umarmw@users.noreply.github.com>
2021-12-04 09:44:48 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+$welcome = "Welcome to the Open Online Class";
+?>
+
+<html>
+    <head>
+        <title>php</title>
+    </head>
+    <body>
+        <h1><?php echo $welcome; ?></h1>
+    </body>
+</html>
